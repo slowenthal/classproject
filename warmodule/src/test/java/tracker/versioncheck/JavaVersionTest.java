ce0da0c61251e4fa1cf2eb7e624667fd621e9ce2
stevelowenthal <steve@datastax.com>
2013-09-21 10:43:20 -0700

    Working standalone jetty with web app (at least on mac).

--- warmodule/src/test/java/tracker/versioncheck/JavaVersionTest.java
+++ warmodule/src/test/java/tracker/versioncheck/JavaVersionTest.java
@@ -15,13 +15,14 @@ import junit.framework.TestCase;
 
 public class JavaVersionTest extends TestCase {
 
-  public static final String MINIMUM_JAVA_VERSION = "1.6";
+  public static final String MINIMUM_JAVA_VERSION = "1.7";
 
   public void testJavaVersion() {
 
     String javaVersion = System.getProperty("java.version");
     System.out.printf("Current Java Version: %s\n", javaVersion);
-    assertTrue("The Java Version must be > " + MINIMUM_JAVA_VERSION, javaVersion.compareTo(MINIMUM_JAVA_VERSION) > 1);
+
+    assertTrue("The Java Version must be > " + MINIMUM_JAVA_VERSION, javaVersion.compareTo(MINIMUM_JAVA_VERSION) > 0);
   }
 
 }
