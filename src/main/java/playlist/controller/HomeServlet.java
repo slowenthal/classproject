9eae369036dae4fe0156f6306c7274c93a9c4c74
stevelowenthal <steve@datastax.com>
2013-10-05 19:27:16 -0700

    Check in Session 1 - remove a couple more things

--- src/main/java/playlist/controller/HomeServlet.java
+++ src/main/java/playlist/controller/HomeServlet.java
@@ -18,10 +18,6 @@ import java.io.IOException;
 
 public class HomeServlet extends HttpServlet {
 
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-  }
-
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     StatisticsDAO.increment_counter("page hits: home");
@@ -33,11 +29,5 @@ public class HomeServlet extends HttpServlet {
     request.setAttribute("cassandra_info", cassandraInfo);
     getServletContext().getRequestDispatcher("/home.jsp").forward(request,response);
 
-
-
   }
-
-
-
-
 }
